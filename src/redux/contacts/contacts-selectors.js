901f72fdf36100156681f09063cf7ff3f220eaa6
Anton Yarosh <on.yaroshanton@gmail.com>
2021-05-08 20:58:20 +0300

    add reselect

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,12 +1,13 @@
+import { createSelector } from "reselect";
+
 export const getFilter = state => state.contacts.filter;
 
 export const getAllContacts = state => state.contacts.items;
 
-export const getVisibleContacts = state => {
-  const contacts = getAllContacts(state);
-  const filter = getFilter(state);
+export const getVisibleContacts = createSelector([getAllContacts, getFilter], (contacts, filter) => {
   const normalizedFilter = filter.toLocaleLowerCase();
 
   return contacts.filter(({ name }) =>
     name.toLocaleLowerCase().includes(normalizedFilter));
-};
+});
+
